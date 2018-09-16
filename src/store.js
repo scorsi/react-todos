7454b0857db5949ce3a59d1f1671f4f21c410e6d
scorsi <sylvain.corsini@gmail.com>
2018-09-17 00:19:05 +0200

    Remove useless code

--- src/store.js
+++ src/store.js
@@ -2,9 +2,18 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/RootReducer';
 
-export default function configureStore(initialState = {}) {
+const persitedState = {
+    todos: [{
+        id: 0,
+        text: 'Welcome back!',
+        completed: false
+    }]
+};
+
+export default function configureStore(initialState = persitedState) {
     return createStore(
         rootReducer,
+        initialState,
         applyMiddleware(thunk)
     );
 }
